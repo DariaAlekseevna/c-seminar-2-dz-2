ce84d5cc9912343ed1ad69f1275cff8043ac5c4b
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 09:57:15 +0300

    решение 13 задачи, у меня не получается вывести нужный элемент массив

---
+++ task 13/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+int n = 0;
+int number1 = number;
+
+if  (number >= 100)
+{
+    while (number1 >= 10)
+    {
+        number1 = number1 / 10;
+        n++;
+    }
+}
+else Console.WriteLine("третьей цифры нет");
+
+int index = 0;
+int[] array = new int [n];
+
+while (number >  10)
+{
+    array[index] = number % 10;
+    index++;
+    number = number / 10;
+}
+
+
+System.Console.WriteLine(array[n-1]);
