dfea32308879857e12094be91af11c22ced24777
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 23:36:09 +0300

    доделала задачу про третью цифру числа

--- task 13/Program.cs
+++ task 13/Program.cs
@@ -3,31 +3,39 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
+int number;
+
 Console.WriteLine("Введите число:");
-int number = int.Parse(Console.ReadLine());
+while (! int.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.WriteLine("Введите корректное значение:");
+}
 
 int n = 0;
 int number1 = number;
 
-if  (number >= 100)
+if (number >= 100)
 {
     while (number1 >= 10)
     {
         number1 = number1 / 10;
         n++;
     }
+
+    int index = 0;
+    int[] array = new int[n];
+
+    while (number > 10)
+    {
+        array[index] = number % 10;
+        index++;
+        number = number / 10;
+    }
+    
+    System.Console.WriteLine(array[n - 2]);
 }
-else Console.WriteLine("третьей цифры нет");
 
-int index = 0;
-int[] array = new int [n];
-
-while (number >  10)
+else
 {
-    array[index] = number % 10;
-    index++;
-    number = number / 10;
+    Console.WriteLine("третьей цифры нет");
 }
-
-
-System.Console.WriteLine(array[n-1]);
